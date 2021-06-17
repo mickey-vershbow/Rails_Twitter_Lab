c48f5c06d0b60e9b36153bd57a8f1e98f603afee
Mickey Vershbow <mickey.vershbow@gmail.com>
2021-06-17 15:09:39 -0400

    Can POST new tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,14 +5,36 @@ def index
     render json: {status: 200, tweets: tweet}
 end
 
-# def show
-#     found_tweet = Tweet.find(params[:id])
-#     if tweet
-#         render(status: 200, json: {tweet: found_tweet})
-#     else
-#         render(status: 404, json: {tweet: found_tweet})
-#     end
-# end
+def show
+    found_tweet = Tweet.find(params[:id])
+    if found_tweet
+        render(status: 200, json: {tweet: found_tweet})
+    else
+        render(status: 404, json: {tweet: found_tweet})
+    end
+end
+
+def create
+    tweet = Tweet.new(tweet_params)
+
+    if tweet.save
+        render(status: 200, json: {tweet: tweet})
+    else
+        render(status: 406, json: {tweet: tweet, errors: tweet.errors})
+    end
+end
+
+def update
+end
+
+def destroy
+end
+
+private
+
+def tweet_params
+    params.require(:tweet).permit(:title, :content, :author)
+end
 
 
 end
