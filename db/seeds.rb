e249fa389aa25c25546a5ab843e0cfef2c1235ac
Mickey Vershbow <mickey.vershbow@gmail.com>
2021-06-16 21:33:44 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+    Tweet.create(
+        title: Faker::Hipster.sentence(word_count: 3),
+        content: Faker::Hipster.sentence,
+        author: Faker::Twitter.screen_name
+    )
+end
+
