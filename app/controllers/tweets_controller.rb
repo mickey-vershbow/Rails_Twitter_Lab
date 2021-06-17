35112a45a34a42a8438a9c068bd12b649a2619a9
Mickey Vershbow <mickey.vershbow@gmail.com>
2021-06-16 21:47:07 -0400

    superclass mismatch on tweets controller

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,18 @@
+class TweetsCotroller < ApplicationController
+
+def index
+    tweet = Tweet.all
+    render json: {status: 200, tweets: tweet}
+end
+
+# def show
+#     found_tweet = Tweet.find(params[:id])
+#     if tweet
+#         render(status: 200, json: {tweet: found_tweet})
+#     else
+#         render(status: 404, json: {tweet: found_tweet})
+#     end
+# end
+
+
+end
